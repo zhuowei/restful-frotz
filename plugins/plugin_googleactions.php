5b21af30d0e431e8842b5b3e95bd6e60b142293b
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2016-12-10 18:44:31 -0500

    Use sha1 instead; it's shorter

--- plugins/plugin_googleactions.php
+++ plugins/plugin_googleactions.php
@@ -7,7 +7,7 @@ function handler_input(array &$params) {
 		$command = "";
 	}
 	$params["command"] = $command;
-	$params["session_id"] = hash("sha256", getUser()["user_id"]);
+	$params["session_id"] = sha1(getUser()["user_id"]);
 	$params["data_id"] = "zork1";
 }
 function handler_output(array $data) : array {
